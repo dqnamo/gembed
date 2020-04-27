cd472bd01cd656c56de9fe74d2d715bef58da0ee
John Paul <johnarpaul@gmail.com>
2020-04-27 21:47:40 +0100

    improved code

--- Gembed.gemspec
+++ Gembed.gemspec
@@ -1,17 +1,13 @@
 Gem::Specification.new do |s|
   s.name        = 'gembed'
-  s.version     = '0.9.0'
+  s.version     = '1.0.4'
   s.date        = '2020-04-22'
   s.summary     = "A gem for embeding media."
   s.description = "Embed media by URL."
   s.authors     = ["John Paul"]
   s.email       = 'hello@johnraj.me'
-  s.files       = ["lib/gembed.rb", "lib/gembed/loom.rb"]
+  s.files       = ["lib/gembed.rb", "lib/gembed/loom.rb", "lib/gembed/youtube.rb"]
   s.homepage    =
-    'https://github.com/iJohnPaul/gembed/'
+    'https://rubygems.org/gems/gembed'
   s.license       = 'MIT'
-  s.metadata = {
-  "source_code_uri"   => "https://github.com/iJohnPaul/gembed/",
-  "wiki_uri"          => "https://github.com/iJohnPaul/gembed/wiki"
-  }
 end
--- lib/gembed/loom.rb
+++ lib/gembed/loom.rb
@@ -1,12 +1,12 @@
 module Gembed
   class Loom
     def self.embed(url)
-      result = "<div style='position: relative; padding-bottom: 62.5%; height: 0;'><iframe src='#{url.gsub("/share/", "/embed/")}'' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen style='position: absolute; top: 0; left: 0; width: 100%; height: 100%;'></iframe></div>"
-      if result.respond_to?(:html_safe)
-        result.html_safe
-      else
-        result
-      end
+      result = "<div style='position: relative; padding-bottom: 62.5%; height: 0;'><iframe src='https://www.loom.com/embed/#{find_id(url)}' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen style='position: absolute; top: 0; left: 0; width: 100%; height: 100%;'></iframe></div>"
+      result.respond_to?(:html_safe) ? result.html_safe : result
+    end
+
+    def self.find_id(url)
+      url.split("share/")[1]
     end
   end
 end
